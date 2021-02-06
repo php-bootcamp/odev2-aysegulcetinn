4056ed27ea55f231f6b03bae3558f4213ab35908
aysegulcetinn <62505241+aysegulcetinn@users.noreply.github.com>
2021-02-06 07:38:32 +0300

    Update app.php

--- app.php
+++ app.php
@@ -5,6 +5,11 @@ $weight = 55; // kilo (kg)
 $height = 170; // boy (cm)
 $age = 25; // yaş (sene)
 
+$erkekler=((10*$weight)+(6.25*$height)-(5*$age)+5);
+  echo"Erkekler için;$erkekler\n";
+$kad=((10*$weight)+(6.25*$height)-(5*$age)-161);
+  echo"Kadınlar için:$kad";
+?>
 /**
  * Bu ödevde günlük kalori hesaplaması yapacağız.
  * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
